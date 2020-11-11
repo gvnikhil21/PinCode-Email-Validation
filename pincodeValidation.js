253cf52b5130a547af164d1b1d40da6ff5b3b1c8
gvnikhil21 <69467502+gvnikhil21@users.noreply.github.com>
2020-11-11 20:56:19 +0530

    Added: Allow space in the mid of the pin code(spcae may or maynot be there) functionality added

--- pincodeValidation.js
+++ pincodeValidation.js
@@ -1,4 +1,4 @@
-const regexPinCode = RegExp(`^[0-9]{6}$`);
+const regexPinCode = RegExp(`^[\\d]{3}[ ]?[\\d]{3}$`);
 
 let validatePinCode = (pinCode) => {
     console.log(`validating ${pinCode}... `);
@@ -27,6 +27,14 @@ try {
 // validate pin code for 400088B
 try {
     pinCode = `400088B`;
+    validatePinCode(pinCode);
+} catch (e) {
+    console.error(e);
+}
+
+// validate pin code for 400 088
+try {
+    pinCode = `400 088`;
     validatePinCode(pinCode);
 } catch (e) {
     console.error(e);
