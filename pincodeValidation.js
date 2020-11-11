79482f7856e5bc4f63c0c4347e894a3567198e17
gvnikhil21 <69467502+gvnikhil21@users.noreply.github.com>
2020-11-11 20:41:31 +0530

    Added: Validate PIN Code 400088 functionality added

--- pincodeValidation.js
+++ pincodeValidation.js
@@ -1,1 +1,16 @@
-console.log("Hello");
+const regexPinCode = RegExp(`[\\d]{6}`);
+
+let validatePinCode = (pinCode) => {
+    if (!regexPinCode.test(pinCode)) throw 'Invalid PIN Code!'
+    {
+        console.log(`PIN Code accepted.`);
+    }
+}
+
+// validate pin code for 400088
+try {
+    pinCode = 400088;
+    validatePinCode(pinCode);
+} catch (e) {
+    console.error(e);
+}
