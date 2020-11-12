f3890e80dff8e2e05886b3aa4221d4b12a034e01
gvnikhil21 <69467502+gvnikhil21@users.noreply.github.com>
2020-11-12 08:47:46 +0530

    Added: Sample valid and invalid emails added to test the generated email-regex

--- emailValidation.js
+++ emailValidation.js
@@ -1,4 +1,4 @@
-const regexEmail = RegExp(`^[\\w]+(?:[_+-\\.][\\w]+)*@(?:[a-z]+\\.){1,2}(?:[a-z]{2,})?$`);
+const regexEmail = RegExp(`^[\\w]+(?:[_+-\\.][\\w]+)*@(?:[\\w]+){1}(?:\\.[a-z]{2,}){1,2}$`);
 
 let validateEmailUtil = (email) => {
     console.log(`validating ${email}...`);
@@ -25,3 +25,31 @@ validateEmail(`abc@gmail.co`);
 validateEmail(`abc.xyz@gmail.co`);
 // validating second optional part
 validateEmail(`abc.xyz@gmail.co.in`);
+
+// testing valid emails
+console.log(`\nSample valid emails test: `);
+validateEmail(`abc@yahoo.com`);
+validateEmail(`abc-100@yahoo.com`);
+validateEmail(`abc.100@yahoo.com`);
+validateEmail(`abc111@abc.com`);
+validateEmail(`abc-100@abc.net`);
+validateEmail(`abc.100@abc.com.au`);
+validateEmail(`abc@1.com`);
+validateEmail(`abc+100@gmail.com`);
+
+// testing invalid emails
+console.log(`\nSample invalid emails test: `);
+validateEmail(`abc`);
+validateEmail(`abc@.com.my`);
+validateEmail(`abc123@gmail.a`);
+validateEmail(`abc123@.com`);
+validateEmail(`abc123@.com.com`);
+validateEmail(`.abc@abc.com`);
+validateEmail(`abc()*@gmail.com`);
+validateEmail(`abc@%*.com`);
+validateEmail(`abc..2002@gmail.com`);
+validateEmail(`abc.@gmail.com`);
+validateEmail(`abc@abc@gmail.com`);
+validateEmail(`abc@gmail.com.1a`);
+validateEmail(`abc@gmail.com.aa.au`);
+validateEmail(`abc@yahoo.com.`);
